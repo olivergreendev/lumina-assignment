e557e61a1908658cbf36e55e99090060c1db9d60
olivergreendev <olivergreendev@gmail.com>
2022-01-25 22:17:43 +0000

    Adjusted the server port to align with Heroku's default port: 8080.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const path = require('path');
 
 app.use(cors());
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -9,20 +9,20 @@ export class UsersService {
   constructor(private http: HttpClient) { }
 
   getUsers() {
-    return this.http.get('http://localhost:3000/api/users');
+    return this.http.get('http://localhost:8080/api/users');
   }
   updateUsersFavouriteMovies(userID: number, movies: string) {
     const body = {movies: movies};
-    return this.http.put(`http://localhost:3000/api/movies/${userID}`, body);
+    return this.http.put(`http://localhost:8080/api/movies/${userID}`, body);
   }
   createUser(firstName: string, lastName: string, favouriteMovies: string) {
-    return this.http.post(`http://localhost:3000/api/user`, {
+    return this.http.post(`http://localhost:8080/api/user`, {
       'firstName': firstName,
       'lastName': lastName,
       'favouriteMovies': favouriteMovies}
     );
   }
   deleteUser(userID: number) {
-    return this.http.delete(`http://localhost:3000/api/user/${userID}`);
+    return this.http.delete(`http://localhost:8080/api/user/${userID}`);
   }
 }
